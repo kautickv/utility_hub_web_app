7d86afaa6b22517ab4a3395fe0a67ba6cedb5a7d
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-04-26 15:47:09 -0500

    Changed main.tf to add access keys

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,6 +1,11 @@
 provider "aws" {
-  region = "us-east-1" # Set your desired AWS region
+  region     = "us-east-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
+
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "front-end-react-soc-landing-3516514"
