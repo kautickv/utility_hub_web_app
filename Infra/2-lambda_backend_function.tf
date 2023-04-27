406a296d027af43a3f9c83eb86c5a5def05537cd
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-04-27 17:35:22 -0500

    Added simple hello word python scrypt. Terraform script to zip and upload backend to s3 bucket. Create a lambda function

--- Infra/2-lambda_backend_function.tf
+++ Infra/2-lambda_backend_function.tf
@@ -1,5 +1,5 @@
 # Create an IAM role to assign to lambda function
-resource "aws_iam_role" "hello_lambda_exec" {
+resource "aws_iam_role" "password-generator-backend-lambda-function_exec" {
   name = "password_generator_lambda_backend_role"
 
   assume_role_policy = <<POLICY
@@ -23,5 +23,46 @@ resource "aws_iam_role_policy_attachment" "hello_lambda_policy" {
   role       = aws_iam_role.hello_lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+
+# Create the lambda fucntion which will handle backend requests
+resource "aws_lambda_function" "password-generator-backend-lambda-function" {
+  function_name = "password-generator-backend-lambda-function"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_hello.key
+
+  runtime = "python3.9"
+  handler = "../back_end/hello_world.py"
+
+  source_code_hash = data.archive_file.password-generator-backend-lambda-function-zip.output_base64sha256
+
+  role = aws_iam_role.password-generator-backend-lambda-function.arn
+}
+
+# Create a cloudwatch log group for lambda execution logs
+resource "aws_cloudwatch_log_group" "password-generator-backend-lambda-function-logs" {
+  name = "/aws/lambda/${aws_lambda_function.password-generator-backend-lambda-function.function_name}"
+
+  retention_in_days = 14
+}
+
+# Zip the current backend python script
+data "archive_file" "password-generator-backend-lambda-function-zip" {
+  type = "zip"
+
+  source_dir  = "../${path.module}/back_end"
+  output_path = "../${path.module}/back_end.zip"
+}
+
+resource "aws_s3_object" "password-generator-backend-lambda-function-object" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "back_end.zip"
+  source = data.archive_file.password-generator-backend-lambda-function-zip.output_path
+
+  etag = filemd5(data.archive_file.password-generator-backend-lambda-function-zip.output_path)
+}
+
 
 
