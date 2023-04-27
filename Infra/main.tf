a41977a757f22af655d306530463c04823c861f0
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-04-26 23:16:55 -0500

    Added backend and configured to push state into s3 bucket

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,7 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket         = "password-generator-terraform-state-management-bucket"
+    key            = "dev/passwordGeneratorTerraformState"
+    region         = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1" # Set your desired AWS region
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "front-end-react-soc-landing-3516514"
+  bucket = "front-end-react-password-generator-3516514"
 }
