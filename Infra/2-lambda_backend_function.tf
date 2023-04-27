6429fb58d0aea7c70f35e8f00e8c21ad17594eeb
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-04-27 17:56:34 -0500

    Changing the path to the handler function for lambda again. Changed to hello_world.lambda_handler

--- Infra/2-lambda_backend_function.tf
+++ Infra/2-lambda_backend_function.tf
@@ -33,7 +33,7 @@ resource "aws_lambda_function" "password-generator-backend-lambda-function" {
   s3_key    = aws_s3_object.password-generator-backend-lambda-function-object.key
 
   runtime = "python3.9"
-  handler = "hello_world.py"
+  handler = "hello_world.lambda_handler"
 
   source_code_hash = data.archive_file.password-generator-backend-lambda-function-zip.output_base64sha256
 
