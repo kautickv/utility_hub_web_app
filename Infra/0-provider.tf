f40f69eacc8660ed235b39d039ccc437b8ee06f6
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-04-27 16:57:56 -0500

    Added backend folder with a file called hello_world.py. Also added some more terraform scripts

--- Infra/0-provider.tf
+++ Infra/0-provider.tf
@@ -18,7 +18,3 @@ terraform {
 provider "aws" {
   region = "us-east-1" # Set your desired AWS region
 }
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "front-end-react-password-generator-3516514"
-}
