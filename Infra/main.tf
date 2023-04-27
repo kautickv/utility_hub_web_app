4be916c8cd73835b3efd217521398b2cbfe789b5
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-04-26 19:36:46 -0500

    Simplified main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,11 +1,6 @@
-provider "AzureCloud" {
+provider "aws" {
   region     = "us-east-1"
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
 }
-
-variable "aws_access_key_id" {}
-variable "aws_secret_access_key" {}
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "front-end-react-soc-landing-3516514"
