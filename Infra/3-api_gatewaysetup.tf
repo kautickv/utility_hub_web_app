fcae793d69ba2881bc0db42810dee340c4bb1f5d
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-04-29 15:20:56 -0500

    Added terraform script to create API gateway

---
+++ Infra/3-api_gatewaysetup.tf
@@ -0,0 +1,35 @@
+resource "aws_apigatewayv2_api" "prod" {
+  name          = "prod"
+  protocol_type = "REST"
+}
+
+resource "aws_apigatewayv2_stage" "dev" {
+  api_id = aws_apigatewayv2_api.prod.id
+
+  name        = "dev"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.prod_api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+resource "aws_cloudwatch_log_group" "prod_api_gw" {
+  name = "/aws/api-gw/${aws_apigatewayv2_api.prod.name}"
+
+  retention_in_days = 14
+}
