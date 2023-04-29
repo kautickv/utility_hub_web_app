5dd9c7a346f1493a6f700ce1acf8c94159d9ccc8
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-04-29 15:58:40 -0500

    Create resource named home in API gateway

--- Infra/3-api_gatewaysetup.tf
+++ Infra/3-api_gatewaysetup.tf
@@ -1,3 +1,10 @@
 resource "aws_api_gateway_rest_api" "password_generator_api_gateway" {
   name = "password_generator_api_gateway"
+  description = "REST API gateway for password_generator app. "
+}
+
+resource "aws_api_gateway_resource" "password_generator_api_gateway_home_resource" {
+  parent_id   = aws_api_gateway_rest_api.password_generator_api_gateway.root_resource_id
+  path_part   = "home"
+  rest_api_id = aws_api_gateway_rest_api.password_generator_api_gateway.id
 }
