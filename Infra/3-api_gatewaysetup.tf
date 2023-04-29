b9129278be6f213db1885f3126f07e31faf3fedd
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-04-29 15:42:13 -0500

    Converting HTTP to REST api for apigateway.

--- Infra/3-api_gatewaysetup.tf
+++ Infra/3-api_gatewaysetup.tf
@@ -1,35 +1,3 @@
-resource "aws_apigatewayv2_api" "password_generator_api_gateway_prod" {
-  name          = "password_generator_api_gateway_prod"
-  protocol_type = "HTTP"
-}
-
-resource "aws_apigatewayv2_stage" "dev" {
-  api_id = aws_apigatewayv2_api.password_generator_api_gateway_prod.id
-
-  name        = "dev"
-  auto_deploy = true
-
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.prod_api_gw.arn
-
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-      }
-    )
-  }
-}
-
-resource "aws_cloudwatch_log_group" "prod_api_gw" {
-  name = "/aws/api-gw/${aws_apigatewayv2_api.password_generator_api_gateway_prod.name}"
-
-  retention_in_days = 14
+resource "aws_api_gateway_rest_api" "password_generator_api_gateway" {
+  name = "password_generator_api_gateway"
 }
