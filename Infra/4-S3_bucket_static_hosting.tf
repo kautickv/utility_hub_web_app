ada5ec3debada3c8da336429342a59b1b343c853
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-08 14:14:13 -0500

    Removing cors configurations

--- Infra/4-S3_bucket_static_hosting.tf
+++ Infra/4-S3_bucket_static_hosting.tf
@@ -3,19 +3,6 @@ resource "aws_s3_bucket" "static_hosting_bucket_name" {
   bucket = "${var.bucket_name}"
 }
 
-# Configure bucket to enable cors
-resource "aws_s3_bucket_cors_configuration" "static_bucket_cors_config" {
-  bucket = aws_s3_bucket.static_hosting_bucket_name.id
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET"]
-    allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }
-}
-
 #Create a json object for s3 bucket policy to make bucket public
 data "aws_iam_policy_document" "s3_read_permissions" {
   statement {
