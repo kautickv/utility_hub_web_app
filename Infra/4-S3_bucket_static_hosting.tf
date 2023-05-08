696a34230fe8f369d11f8cd5bc90e1ae1d1d33f1
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-08 16:11:53 -0500

    Added permission to edit bucket itself

--- Infra/4-S3_bucket_static_hosting.tf
+++ Infra/4-S3_bucket_static_hosting.tf
@@ -3,16 +3,6 @@ resource "aws_s3_bucket" "static_hosting_bucket_name" {
   bucket = "${var.bucket_name}"
 }
 
-# Create and assign a bucket policy to block all public access to s3 bucket
-resource "aws_s3_bucket_public_access_block" "static_hosting_bucket_name" {
-  bucket = aws_s3_bucket.static_hosting_bucket_name.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 #Create a json object for s3 bucket policy to make bucket public
 data "aws_iam_policy_document" "s3_read_permissions" {
   statement {
@@ -25,7 +15,9 @@ data "aws_iam_policy_document" "s3_read_permissions" {
       "s3:GetObject"
     ]
 
-    resources = ["${aws_s3_bucket.static_hosting_bucket_name.arn}/*"]
+    resources = ["${aws_s3_bucket.static_hosting_bucket_name.arn}/*",
+                 "${aws_s3_bucket.static_hosting_bucket_name.arn}"
+    ]
   }
 }
 
