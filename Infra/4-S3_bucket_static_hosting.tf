c98f397d5a36e31275182e5962bfbfe1b464b798
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-08 17:40:44 -0500

    recreate all s3 static resources

---
+++ Infra/4-S3_bucket_static_hosting.tf
@@ -0,0 +1,53 @@
+#Create bucket to store React build files
+resource "aws_s3_bucket" "static_hosting_bucket_name" {
+  bucket = "${var.bucket_name}"
+  force_destroy = true
+}
+
+# Create and assign a bucket policy to unblock all public access to s3 bucket
+resource "aws_s3_bucket_public_access_block" "static_hosting_bucket_name" {
+  bucket = aws_s3_bucket.static_hosting_bucket_name.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Configure s3 bucket for static hosting
+resource "aws_s3_bucket_website_configuration" "static_hosting_bucket_config" {
+    bucket = aws_s3_bucket.static_hosting_bucket_name.id
+
+    index_document {
+        suffix = "index.html"
+    }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+#Create a json object for s3 bucket policy to make bucket public
+data "aws_iam_policy_document" "s3_read_permissions" {
+  statement {
+    sid = "AllowPublicReadAccess"
+    effect = "Allow"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = ["${aws_s3_bucket.static_hosting_bucket_name.arn}",
+                 "${aws_s3_bucket.static_hosting_bucket_name.arn}/*"
+    ]
+  }
+}
+
+# Assign the above policy to s3 static hosting bucket
+resource "aws_s3_bucket_policy" "s3_allow_public_access" {
+  bucket = aws_s3_bucket.static_hosting_bucket_name.id
+  policy = data.aws_iam_policy_document.s3_read_permissions.json
+}
