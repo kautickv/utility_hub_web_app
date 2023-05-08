f4a7c56bcac99e5bd513b2a171dd63fb044a5a37
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-08 13:57:33 -0500

    Make s3 static bucket hosting public

--- Infra/4-S3_bucket_static_hosting.tf
+++ Infra/4-S3_bucket_static_hosting.tf
@@ -1,7 +1,9 @@
+#Create bucket to store React build files
 resource "aws_s3_bucket" "static_hosting_bucket_name" {
   bucket = "${var.bucket_name}"
 }
 
+# Configure bucket to enable cors
 resource "aws_s3_bucket_cors_configuration" "static_bucket_cors_config" {
   bucket = aws_s3_bucket.static_hosting_bucket_name.id
 
@@ -12,4 +14,23 @@ resource "aws_s3_bucket_cors_configuration" "static_bucket_cors_config" {
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
+}
+
+#Create a json object for s3 bucket policy to make bucket public
+data "aws_iam_policy_document" "s3_read_permissions" {
+  statement {
+    effect = "Allow"
+    principal = "*"
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = ["${aws_s3_bucket.static_hosting_bucket_name.arn}/*"]
+  }
+}
+
+# Assign the above policy to s3 static hosting bucket
+resource "aws_s3_bucket_policy" "s3_allow_public_access" {
+  bucket = aws_s3_bucket.static_hosting_bucket_name.id
+  policy = data.aws_iam_policy_document.s3_read_permissions.json
 }
