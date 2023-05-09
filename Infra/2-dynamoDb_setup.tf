e78091515c6d4aa23c48eb50d4705ee7facede23
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-09 18:33:00 -0500

    Adding global secondary index for each attribute in dynamoDb table

--- Infra/2-dynamoDb_setup.tf
+++ Infra/2-dynamoDb_setup.tf
@@ -23,4 +23,35 @@ resource "aws_dynamodb_table" "sign_in_user_table" {
     name = "lastLogout"
     type = "S"
   }
+  global_secondary_index {
+    name               = "FirstNameIndex"
+    hash_key           = "first_name"
+    projection_type    = "ALL"
+    write_capacity     = 1
+    read_capacity      = 1
+  }
+
+  global_secondary_index {
+    name               = "LastNameIndex"
+    hash_key           = "last_name"
+    projection_type    = "ALL"
+    write_capacity     = 1
+    read_capacity      = 1
+  }
+
+  global_secondary_index {
+    name               = "JwtTokenIndex"
+    hash_key           = "jwtToken"
+    projection_type    = "ALL"
+    write_capacity     = 1
+    read_capacity      = 1
+  }
+
+  global_secondary_index {
+    name               = "LastLogoutIndex"
+    hash_key           = "lastLogout"
+    projection_type    = "ALL"
+    write_capacity     = 1
+    read_capacity      = 1
+  }
 }
