4f34c37ebd14423e9f2759b9931c5585a0e4a147
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-09 16:22:10 -0500

    Changed SSL ARN and added a new variable for the hosted zone id

---
+++ Infra/6-route53_record_setup.tf
@@ -0,0 +1,31 @@
+# Create a hosted zone with domain name
+resource "aws_route53_zone" "main" {
+  name = var.domain_name
+  tags = var.common_tags
+}
+
+# Add cloudfront distribution domain name as a record.
+resource "aws_route53_record" "root-a" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.root_s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+# Add another record for www.{domain_name}
+resource "aws_route53_record" "www-a" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.www_s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
--- Infra/terraform.tfvars
+++ Infra/terraform.tfvars
@@ -1,7 +1,9 @@
 domain_name = "passwordgenerator.vaisnavsingkautick.com"
 bucket_name = "passwordgenerator.vaisnavsingkautick.com"
 
-SSL_certificate_arn = "arn:aws:acm:us-east-1:324277551001:certificate/d16d4c49-789a-4e31-80b2-414c1832169f"
+SSL_certificate_arn = "arn:aws:acm:us-east-1:324277551001:certificate/7530acc1-af92-4c4f-8f0d-e10edfb29f69"
+hosted_zone_id = "Z0236168HPT8WHQWIY9"
+
 common_tags = {
   Project = "passwordgenerator.vaisnavsingkautick.com"
 }
