5b939d5c2607bd712b1b81f35b2d5451fc720505
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-09 11:18:55 -0500

    Created cloudfornt distribution for s3 static hosting bucket

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -10,6 +10,11 @@ variable "bucket_name" {
     description = "The name of the bucket with the www. prefix. Same as domain name"
 }
 
+variable "SSL_certificate_arn" {
+  type = string
+  description = "The ARN for the SSL certificate to be used by cloudfront. Terraform script does not create a request certificate"
+}
+
 variable "common_tags"{
     description = "Common tags you want to apply to all components"
 }
