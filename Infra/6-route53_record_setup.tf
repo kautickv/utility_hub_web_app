16fc16aa09596296a1e6ab5030c36032d1d39962
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-09 16:29:09 -0500

    Only adding one record to route53

--- Infra/6-route53_record_setup.tf
+++ Infra/6-route53_record_setup.tf
@@ -6,21 +6,8 @@ resource "aws_route53_record" "root-a" {
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.root_s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-# Add another record for www.{domain_name}
-resource "aws_route53_record" "www-a" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "www.${var.domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.www_s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
