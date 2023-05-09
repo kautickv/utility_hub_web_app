600220b1fb4ec879ee400a486c43b0b12b75b1aa
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-09 11:30:10 -0500

    Fixed naming errors in cloudfront terraform file

--- Infra/5-cloudfront_distribution_setup.tf
+++ Infra/5-cloudfront_distribution_setup.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = aws_s3_bucket.static_hosting_bucket_name.website_endpoint
+    domain_name              = aws_s3_bucket_website_configuration.static_hosting_bucket_config.website_endpoint
     origin_id                = "S3-.${var.bucket_name}"
 
     custom_origin_config {
@@ -36,29 +36,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     default_ttl            = 3600
     max_ttl                = 86400
     compress               = true
-  }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/content/immutable/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
   }
 
   price_class = "PriceClass_200"
