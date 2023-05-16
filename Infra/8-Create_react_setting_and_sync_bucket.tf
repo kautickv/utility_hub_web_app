1cbde7a564ac4f34a7cd3c8774b370f581f1ba13
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-16 17:38:16 -0500

    Changing back to .env file

--- Infra/8-Create_react_setting_and_sync_bucket.tf
+++ Infra/8-Create_react_setting_and_sync_bucket.tf
@@ -5,7 +5,7 @@
 # Create .env and store in front_end folder. Contains API gateway invoke url, s3 website endpoint.
 resource "local_file" "react_env_file" {
 
-  filename = "../${path.module}/front_end/setting.txt"
+  filename = "../${path.module}/front_end/.env"
   content  = <<-EOF
     REACT_APP_API_GATEWAY_BASE_URL=aws_api_gateway_stage.password_generator_api_gateway_stage.invoke_url
     REACT_APP_BUCKET_HOSTING_ID = aws_s3_bucket.static_hosting_bucket_name.id
@@ -22,6 +22,6 @@ resource "null_resource" "list_directory" {
   }
 
   provisioner "local-exec" {
-    command = "cd ../${path.module}/front_end/ && ls"
+    command = "cd ../${path.module}/front_end/ && ls -a"
   }
 }
