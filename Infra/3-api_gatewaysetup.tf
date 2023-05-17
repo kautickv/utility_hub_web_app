f7601d4c25d1824f1580df8f7df2ac5f7c783351
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-16 21:27:01 -0500

    Added options method for get /home

--- Infra/3-api_gatewaysetup.tf
+++ Infra/3-api_gatewaysetup.tf
@@ -3,15 +3,58 @@ resource "aws_api_gateway_rest_api" "password_generator_api_gateway" {
   name = "${var.app_name}_api_gateway"
   description = "REST API gateway for ${var.app_name} app. "
 
-  # Enable CORS
-  cors_configuration {
-    allow_origin      = "*"
-    allow_headers     = "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
-    allow_methods     = "GET,POST,OPTIONS"
-    expose_headers    = "x-amzn-Remapped-Access-Control-Allow-*"
-    max_age_seconds   = 3600
+}
+
+# Add an OPTIONS method to "home" resource for CORS
+resource "aws_api_gateway_method" "options_home_method" {
+  rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
+  resource_id   = aws_api_gateway_resource.password_generator_api_gateway_home_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+# Add a method response for the OPTIONS method
+resource "aws_api_gateway_method_response" "success_options_home_method" {
+  rest_api_id = aws_api_gateway_rest_api.password_generator_api_gateway.id
+  resource_id = aws_api_gateway_resource.password_generator_api_gateway_home_resource.id
+  http_method = aws_api_gateway_method.options_home_method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = false,
+    "method.response.header.Access-Control-Allow-Methods" = false,
+    "method.response.header.Access-Control-Allow-Origin"  = false,
   }
 }
+
+# Add an integration response for the OPTIONS method
+resource "aws_api_gateway_integration" "options_home_integration" {
+  rest_api_id = aws_api_gateway_rest_api.password_generator_api_gateway.id
+  resource_id = aws_api_gateway_resource.password_generator_api_gateway_home_resource.id
+  http_method = aws_api_gateway_method.options_home_method.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_home_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.password_generator_api_gateway.id
+  resource_id = aws_api_gateway_resource.password_generator_api_gateway_home_resource.id
+  http_method = aws_api_gateway_method.options_home_method.http_method
+  status_code = aws_api_gateway_method_response.success_options_home_method.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
 
 # Add a resource to that API gateway called home
 resource "aws_api_gateway_resource" "password_generator_api_gateway_home_resource" {
