8bba05b6018598318fa065e36fe42e8113ad4c4c
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-16 23:13:30 -0500

    Modify lambda to return json instead of a dict object

--- back_end/hello_world.py
+++ back_end/hello_world.py
@@ -1,10 +1,12 @@
+import json
+
 def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': {
+        'body': json.dumps({
             "Message": "Success"
-        },
+        }),
         'headers' : {
             'Access-Control-Allow-Origin' : '*'
         }
