e47e1ed7598dae0756f9142ced8810145bd3ec7d
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-16 22:02:22 -0500

    Reverting all changes

--- Infra/3-api_gatewaysetup.tf
+++ Infra/3-api_gatewaysetup.tf
@@ -5,57 +5,6 @@ resource "aws_api_gateway_rest_api" "password_generator_api_gateway" {
 
 }
 
-# Add an OPTIONS method to "home" resource for CORS
-resource "aws_api_gateway_method" "options_home_method" {
-  rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
-  resource_id   = aws_api_gateway_resource.password_generator_api_gateway_home_resource.id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
-
-# Add a method response for the OPTIONS method
-resource "aws_api_gateway_method_response" "success_options_home_method" {
-  rest_api_id = aws_api_gateway_rest_api.password_generator_api_gateway.id
-  resource_id = aws_api_gateway_resource.password_generator_api_gateway_home_resource.id
-  http_method = aws_api_gateway_method.options_home_method.http_method
-  status_code = "200"
-  response_models = {
-    "application/json" = "Empty"
-  }
-  
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = false,
-    "method.response.header.Access-Control-Allow-Methods" = false,
-    "method.response.header.Access-Control-Allow-Origin"  = false,
-  }
-}
-
-# Add an integration response for the OPTIONS method
-resource "aws_api_gateway_integration" "options_home_integration" {
-  rest_api_id = aws_api_gateway_rest_api.password_generator_api_gateway.id
-  resource_id = aws_api_gateway_resource.password_generator_api_gateway_home_resource.id
-  http_method = aws_api_gateway_method.options_home_method.http_method
-  type        = "MOCK"
-
-  request_templates = {
-    "application/json" = "{\"statusCode\": 200}"
-  }
-}
-
-resource "aws_api_gateway_integration_response" "options_home_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.password_generator_api_gateway.id
-  resource_id = aws_api_gateway_resource.password_generator_api_gateway_home_resource.id
-  http_method = aws_api_gateway_method.options_home_method.http_method
-  status_code = aws_api_gateway_method_response.success_options_home_method.status_code
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'",
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-}
-
-
 # Add a resource to that API gateway called home
 resource "aws_api_gateway_resource" "password_generator_api_gateway_home_resource" {
   parent_id   = aws_api_gateway_rest_api.password_generator_api_gateway.root_resource_id
@@ -71,29 +20,6 @@ resource "aws_api_gateway_method" "get_home_method" {
   rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
 }
 
-# Add a method response for the GET method
-resource "aws_api_gateway_method_response" "success_get_home_method" {
-  rest_api_id = aws_api_gateway_rest_api.password_generator_api_gateway.id
-  resource_id = aws_api_gateway_resource.password_generator_api_gateway_home_resource.id
-  http_method = aws_api_gateway_method.get_home_method.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-}
-
-# Add an integration response for the GET method
-resource "aws_api_gateway_integration_response" "get_home_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.password_generator_api_gateway.id
-  resource_id = aws_api_gateway_resource.password_generator_api_gateway_home_resource.id
-  http_method = aws_api_gateway_method.get_home_method.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-  }
-}
 
 # Add permissions for api gateway to invoke lambda
 resource "aws_lambda_permission" "lambda_permission" {
@@ -134,12 +60,6 @@ resource "aws_api_gateway_deployment" "password_generator_api_gateway_deployment
       aws_api_gateway_resource.password_generator_api_gateway_home_resource.id,
       aws_api_gateway_method.get_home_method.id,
       aws_api_gateway_integration.get_home_integration.id,
-      aws_api_gateway_method_response.success_get_home_method.id,
-      aws_api_gateway_integration_response.get_home_integration_response.id,
-      aws_api_gateway_method.options_home_method.id,
-      aws_api_gateway_method_response.success_options_home_method.id,
-      aws_api_gateway_integration.options_home_integration.id,
-      aws_api_gateway_integration_response.options_home_integration_response.id,
     ]))
   }
 
