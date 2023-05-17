7c50d372d725175ebca36e869350422a884ff744
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-16 22:47:46 -0500

    Fixed typo again

--- Infra/3-api_gatewaysetup.tf
+++ Infra/3-api_gatewaysetup.tf
@@ -25,9 +25,6 @@ resource "aws_api_gateway_method_response" "options_200" {
     resource_id   = aws_api_gateway_resource.password_generator_api_gateway_home_resource.id
     http_method   = aws_api_gateway_method.options_method.http_method
     status_code   = "200"
-    response_models {
-        "application/json" = "Empty"
-    }
     response_parameters {
         "method.response.header.Access-Control-Allow-Headers" = true,
         "method.response.header.Access-Control-Allow-Methods" = true,
