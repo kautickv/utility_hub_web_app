84ffc335b09efc99e5bfe4d61f52de727c496aa9
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-22 17:24:10 -0500

    Fix error. GET, POST methods were not addded

--- Infra/3-api_gatewaysetup.tf
+++ Infra/3-api_gatewaysetup.tf
@@ -164,6 +164,38 @@ resource "aws_api_gateway_method" "options_creds_method" {
   authorization = "NONE"
 }
 
+# Add a GET method to "creds" resource ("/auth/creds")
+resource "aws_api_gateway_method" "get_creds_method" {
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.password_generator_api_gateway_creds_resource.id
+  rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
+}
+
+resource "aws_api_gateway_method_response" "get_creds_method_response_200" {
+  rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
+  resource_id   = aws_api_gateway_resource.password_generator_api_gateway_creds_resource.id
+  http_method   = aws_api_gateway_method.get_creds_method.http_method
+  status_code   = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+  depends_on = [aws_api_gateway_method.get_creds_method]
+}
+
+# Integrate the GET method for "creds" with our backend lambda function created earlier
+resource "aws_api_gateway_integration" "get_creds_integration" {
+  http_method             = aws_api_gateway_method.get_creds_method.http_method
+  resource_id             = aws_api_gateway_resource.password_generator_api_gateway_creds_resource.id
+  rest_api_id             = aws_api_gateway_rest_api.password_generator_api_gateway.id
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.password-generator-backend-lambda-function.invoke_arn
+}
+
 resource "aws_api_gateway_method_response" "options_creds_method_response_200" {
   rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
   resource_id   = aws_api_gateway_resource.password_generator_api_gateway_creds_resource.id
@@ -206,6 +238,38 @@ resource "aws_api_gateway_resource" "password_generator_api_gateway_verify_resou
   parent_id   = aws_api_gateway_resource.password_generator_api_gateway_auth_resource.id
   path_part   = "verify"
   rest_api_id = aws_api_gateway_rest_api.password_generator_api_gateway.id
+}
+
+# Add a POST method to "verify" resource ("/auth/verify")
+resource "aws_api_gateway_method" "post_verify_method" {
+  authorization = "NONE"
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.password_generator_api_gateway_verify_resource.id
+  rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
+}
+
+resource "aws_api_gateway_method_response" "post_verify_method_response_200" {
+  rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
+  resource_id   = aws_api_gateway_resource.password_generator_api_gateway_verify_resource.id
+  http_method   = aws_api_gateway_method.post_verify_method.http_method
+  status_code   = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+  depends_on = [aws_api_gateway_method.post_verify_method]
+}
+
+# Integrate the POST method for "verify" with our backend lambda function created earlier
+resource "aws_api_gateway_integration" "post_verify_integration" {
+  http_method             = aws_api_gateway_method.post_verify_method.http_method
+  resource_id             = aws_api_gateway_resource.password_generator_api_gateway_verify_resource.id
+  rest_api_id             = aws_api_gateway_rest_api.password_generator_api_gateway.id
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.password-generator-backend-lambda-function.invoke_arn
 }
 
 # Add Options method for verify resource
@@ -260,6 +324,38 @@ resource "aws_api_gateway_resource" "password_generator_api_gateway_login_resour
   rest_api_id = aws_api_gateway_rest_api.password_generator_api_gateway.id
 }
 
+# Add a POST method to "login" resource ("/auth/login")
+resource "aws_api_gateway_method" "post_login_method" {
+  authorization = "NONE"
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.password_generator_api_gateway_login_resource.id
+  rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
+}
+
+resource "aws_api_gateway_method_response" "post_login_method_response_200" {
+  rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
+  resource_id   = aws_api_gateway_resource.password_generator_api_gateway_login_resource.id
+  http_method   = aws_api_gateway_method.post_login_method.http_method
+  status_code   = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+  depends_on = [aws_api_gateway_method.post_login_method]
+}
+
+# Integrate the POST method for "login" with our backend lambda function created earlier
+resource "aws_api_gateway_integration" "post_login_integration" {
+  http_method             = aws_api_gateway_method.post_login_method.http_method
+  resource_id             = aws_api_gateway_resource.password_generator_api_gateway_login_resource.id
+  rest_api_id             = aws_api_gateway_rest_api.password_generator_api_gateway.id
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.password-generator-backend-lambda-function.invoke_arn
+}
+
 # Add Options method for login resource
 resource "aws_api_gateway_method" "options_login_method" {
   rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
@@ -311,6 +407,39 @@ resource "aws_api_gateway_resource" "password_generator_api_gateway_logout_resou
   path_part   = "logout"
   rest_api_id = aws_api_gateway_rest_api.password_generator_api_gateway.id
 }
+
+# Add a POST method to "logout" resource ("/auth/logout")
+resource "aws_api_gateway_method" "post_logout_method" {
+  authorization = "NONE"
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.password_generator_api_gateway_logout_resource.id
+  rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
+}
+
+resource "aws_api_gateway_method_response" "post_logout_method_response_200" {
+  rest_api_id   = aws_api_gateway_rest_api.password_generator_api_gateway.id
+  resource_id   = aws_api_gateway_resource.password_generator_api_gateway_logout_resource.id
+  http_method   = aws_api_gateway_method.post_logout_method.http_method
+  status_code   = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+  depends_on = [aws_api_gateway_method.post_logout_method]
+}
+
+# Integrate the POST method for "logout" with our backend lambda function created earlier
+resource "aws_api_gateway_integration" "post_logout_integration" {
+  http_method             = aws_api_gateway_method.post_logout_method.http_method
+  resource_id             = aws_api_gateway_resource.password_generator_api_gateway_logout_resource.id
+  rest_api_id             = aws_api_gateway_rest_api.password_generator_api_gateway.id
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.password-generator-backend-lambda-function.invoke_arn
+}
+
 
 # Add Options method for logout resource
 resource "aws_api_gateway_method" "options_logout_method" {
