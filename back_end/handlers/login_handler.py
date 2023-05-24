b0f7fa51aa916ddfdee58debbd517afe0f7df0f9
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-05-24 13:01:57 -0500

    Refactor login.handler file

--- back_end/handlers/login_handler.py
+++ back_end/handlers/login_handler.py
@@ -9,11 +9,13 @@ def login_handler(body):
     # Create an instance of GoogleAuth
     auth = GoogleAuth()
     # It should use the code and exchange it for a token and get user information.
-    token = auth.exchange_code_for_token(body)
+    if (not auth.exchange_code_for_token(body)):
+        return buildResponse(500, {"message":"Internal server error"})
     
     # Get user information from access tokens
-    userInfo = auth.get_user_info_from_google(token)
-    print(userInfo)
+    if (not auth.get_user_info_from_google()):
+        return buildResponse(500, {"message":"Internal server error"})
+    
     #Add those user information in the database
     # Generate a jwt token and set it as the response header/or return in http body.
     return buildResponse(200, {"message": "OK"})
