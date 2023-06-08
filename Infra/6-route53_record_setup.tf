207e76cf8dcb674d6a7b2167f5a98356619d7a2d
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-06-08 16:55:45 -0500

    Create hosted zone

--- Infra/6-route53_record_setup.tf
+++ Infra/6-route53_record_setup.tf
@@ -1,4 +1,4 @@
-#Assuming a hosted zone is already created and hosted zone id is set as variable.
+#Need to create a hosted zone
 # Add cloudfront distribution domain name as a record.
 resource "aws_route53_record" "root-a" {
   zone_id = var.hosted_zone_id
