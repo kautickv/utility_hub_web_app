eca01d0c306b5297f6cca996d35a3d3e2a1f0d03
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-06-09 15:20:43 -0500

    Adding TF to deploy a SSL certificate for domain name

--- Infra/5-create_SSL_certificate.tf
+++ Infra/5-create_SSL_certificate.tf
@@ -1,22 +1,28 @@
-# Create a new SSL certificate
+# Create a SSL Certificate
 resource "aws_acm_certificate" "ssl_certificate" {
-  domain_name               = var.domain_name
-  validation_method         = "DNS"
+  provider = "aws.acm"
+  domain_name = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-# Create new record in route53
-resource "aws_route53_record" "cert_records" {
-  for_each = toset([for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.resource_record_name])
-
-  name    = aws_acm_certificate.ssl_certificate.domain_validation_options[each.key].resource_record_name
-  type    = aws_acm_certificate.ssl_certificate.domain_validation_options[each.key].resource_record_type
+# Store the DNS Entries
+resource "aws_route53_record" "validation" {
   zone_id = aws_route53_zone.hosted_zone.zone_id
-  records = [aws_acm_certificate.ssl_certificate.domain_validation_options[each.key].resource_record_value]
-  ttl     = 60
+  name = aws_acm_certificate.ssl_certificate.domain_validation_options.0.resource_record_name
+  type = aws_acm_certificate.ssl_certificate.domain_validation_options.0.resource_record_type
+  records = [aws_acm_certificate.ssl_certificate.domain_validation_options.0.resource_record_value]
+  ttl = "300"
 }
 
-# Validate the certificate
-resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = aws_acm_certificate.ssl_certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_records : record.fqdn]
+# Connects DNS Records with our certificate
+resource "aws_acm_certificate_validation" "default" {
+  provider = "aws.acm"
+  certificate_arn = aws_acm_certificate.ssl_certificate.arn
+  validation_record_fqdns = [
+    aws_route53_record.validation.fqdn,
+  ]
 }
