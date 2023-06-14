47425b3e927095299233c344894818646e94e33c
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-06-14 11:23:20 -0500

    Made s3 static hosting bucket private and enabled acl access

--- Infra/4-S3_bucket_static_hosting.tf
+++ Infra/4-S3_bucket_static_hosting.tf
@@ -2,16 +2,7 @@
 resource "aws_s3_bucket" "static_hosting_bucket_name" {
   bucket = "${var.bucket_name}"
   force_destroy = true
-}
-
-# Create and assign a bucket policy to unblock all public access to s3 bucket
-resource "aws_s3_bucket_public_access_block" "static_hosting_bucket_name" {
-  bucket = aws_s3_bucket.static_hosting_bucket_name.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  acl = "private"
 }
 
 # Configure s3 bucket for static hosting
@@ -25,31 +16,6 @@ resource "aws_s3_bucket_website_configuration" "static_hosting_bucket_config" {
   error_document {
     key = "index.html"
   }
-}
-
-#Create a json object for s3 bucket policy to make bucket public
-data "aws_iam_policy_document" "s3_read_permissions" {
-  statement {
-    sid = "AllowPublicReadAccess"
-    effect = "Allow"
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    actions = [
-      "s3:GetObject"
-    ]
-
-    resources = ["${aws_s3_bucket.static_hosting_bucket_name.arn}",
-                 "${aws_s3_bucket.static_hosting_bucket_name.arn}/*"
-    ]
-  }
-}
-
-# Assign the above policy to s3 static hosting bucket
-resource "aws_s3_bucket_policy" "s3_allow_public_access" {
-  bucket = aws_s3_bucket.static_hosting_bucket_name.id
-  policy = data.aws_iam_policy_document.s3_read_permissions.json
 }
 
 # Print the bucket id
