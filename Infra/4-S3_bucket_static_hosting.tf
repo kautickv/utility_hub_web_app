d6ab1ee19d9545f4d908de7dc93beb5d4ae6e4f3
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-06-14 12:21:07 -0500

    Using canonical usr ID in s3 bucket policy

--- Infra/4-S3_bucket_static_hosting.tf
+++ Infra/4-S3_bucket_static_hosting.tf
@@ -29,7 +29,7 @@ data "aws_iam_policy_document" "s3_allow_cloudfront_access" {
     effect = "Allow"
     principals {
       type = "CanonicalUser"
-      identifiers = [aws_cloudfront_origin_access_identity.static_hosting_oai.iam_arn]
+      identifiers = [aws_cloudfront_origin_access_identity.static_hosting_oai.s3_canonical_user_id]
     }
     actions = [
       "s3:GetObject"
