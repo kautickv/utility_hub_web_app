d81521d7f8761174cd87d3c555ebb067a85e3b44
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-06-14 11:38:57 -0500

    Created a Cloudfront Origin Access Identity for the s3 static hosting bucket

--- Infra/4-S3_bucket_static_hosting.tf
+++ Infra/4-S3_bucket_static_hosting.tf
@@ -18,6 +18,36 @@ resource "aws_s3_bucket_website_configuration" "static_hosting_bucket_config" {
   }
 }
 
+# Create a Cloudfront origin access identity
+resource "aws_cloudfront_origin_access_identity" "static_hosting_oai" {
+  comment = "Access identity for static hosting"
+}
+
+#Create a json object for s3 bucket policy to make bucket public
+data "aws_iam_policy_document" "s3_allow_cloudfront_access" {
+  statement {
+    sid = "AllowCloudfrontAccess"
+    effect = "Allow"
+    principals {
+      type = "CanonicalUser"
+      identifiers = [aws_cloudfront_origin_access_identity.static_hosting_oai.iam_arn]
+    }
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+                 "${aws_s3_bucket.static_hosting_bucket_name.arn}/*"
+    ]
+  }
+}
+
+# Assign the above policy to s3 static hosting bucket
+resource "aws_s3_bucket_policy" "s3_allow_public_access" {
+  bucket = aws_s3_bucket.static_hosting_bucket_name.id
+  policy = data.aws_iam_policy_document.s3_allow_cloudfront_access.json
+}
+
 # Print the bucket id
 output "S3_bucket_hosting_id" {
   value = aws_s3_bucket.static_hosting_bucket_name.id
