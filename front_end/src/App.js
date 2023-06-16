4bb65894d7c75b45fd329ec204948fe7092899d2
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-06-16 17:48:05 -0500

    Convert back to BrowserRouter

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./pages/home";
 import Login from "./pages/login"
-import { HashRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
 
@@ -9,13 +9,13 @@ function App() {
 
 
     <div className="App" style={{backgroundColor: "#f2f2f2"}}>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route exact path='/' element={<Home/>}/>
           <Route exact path='/login' element={<Login/>}/>
           <Route path='/home' element={<Home/>}/>
         </Routes>
-     </HashRouter>
+     </BrowserRouter>
     </div>
   );
 }
