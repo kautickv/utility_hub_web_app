f0534504f72a831c3619fc9261acccac2ed924e0
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-06-23 15:24:06 -0500

    Fixed Typo. Added a custom response body to default action in WAF

--- Infra/10-waf_configuration.tf
+++ Infra/10-waf_configuration.tf
@@ -18,8 +18,9 @@ resource "aws_wafv2_web_acl" "waf_web_acl" {
 
   default_action {
     block {
-      custom_response = {
-        response_code="403"
+      custom_response {
+        response_code = 403
+        custom_response_body_key = aws_wafv2_custom_response_body.custom_response_body.name
       }
     }
   }
