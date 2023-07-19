a98fb3729daa85c8175ad9c804f75ade9be78df4
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-07-19 17:54:39 -0500

    Implement backend. Multitab backend can now respond with JSON

--- back_end/multitab_service/index.py
+++ back_end/multitab_service/index.py
@@ -1,8 +1,13 @@
 from utils.util import buildResponse
+from utils.util import verifyAuthStatus
+from handlers.handlePostMultitab import handlePostMultitab
 
 def lambda_handler(event, context):
     print(event)
     http_method = event['httpMethod']
     path = event['path']
 
-    return buildResponse(200, "OK")
+    if http_method == 'POST' and path =='/multitab':
+        return handlePostMultitab(event, context)
+    else:
+        return buildResponse(404, "Resource not found")
