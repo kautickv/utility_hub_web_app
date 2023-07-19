d9a1de57253d037af653f66b8c2c1568907fbcb4
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-07-19 14:44:09 -0500

    Added UI for MultiTab Opener, tiles and search

---
+++ front_end/src/components/MultiTabOpener/Tile.js
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Link from "@mui/material/Link";
+import { styled } from "@mui/system";
+
+const StyledCard = styled(Card)({
+  maxWidth: 345,
+  "& .MuiCardContent-root": {
+    "& .MuiTypography-root": {
+      // ⚠️ object-fit is not supported by IE11.
+      objectFit: "cover",
+    },
+  },
+});
+
+function Tile(props) {
+  const [configureOpen, setConfigureOpen] = useState(false);
+  const [deleteOpen, setDeleteOpen] = useState(false);
+
+  function handleConfigureOpen(){
+    setConfigureOpen(true);
+  }
+
+  function handleConfigureClose(){
+    setConfigureOpen(false);
+  }
+
+  function handleDeleteOpen(){
+    setDeleteOpen(true);
+  }
+
+  function handleDeleteClose(){
+    setDeleteOpen(false);
+  }
+
+  function handleConfigureSave(){
+    console.log("Configuration saved");
+
+    handleConfigureClose()
+  }
+
+  function handleDelete(){
+    console.log("Tile has been deleted.")
+
+    handleDeleteClose();
+  }
+
+  function handleOpenAll(){
+    console.log(props.urls)
+    props.urls.forEach(urlObj => {
+        window.open(urlObj.url, '_blank');
+      });
+  }
+
+  return (
+    <StyledCard>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={handleConfigureOpen}>Configure</Button>
+        <Button size="small" onClick={handleDeleteOpen}>Delete</Button>
+        <Button size="medium" onClick={handleOpenAll}>Open All</Button>
+      </CardActions>
+
+      <Dialog open={configureOpen} onClose={handleConfigureClose}>
+        <DialogTitle>{"Links"}</DialogTitle>
+        <DialogContent>
+          <List>
+            {props.urls.map((item, index) => (
+              <ListItem key={index}>
+                {item.urlTitle} -{" "}
+                <Link href={item.url} target="_blank" rel="noopener">
+                  {item.url}
+                </Link>
+              </ListItem>
+            ))}
+          </List>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleConfigureClose}>Cancel</Button>
+          <Button onClick={handleConfigureSave}>Save</Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog open={deleteOpen} onClose={handleDeleteClose}>
+        <DialogTitle>{"Delete"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete this tile?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteClose}>Cancel</Button>
+          <Button onClick={handleDelete}>Delete</Button>
+        </DialogActions>
+      </Dialog>
+
+
+    </StyledCard>
+  );
+}
+
+export default Tile;
--- front_end/src/components/MultiTabOpener/index.js
+++ front_end/src/components/MultiTabOpener/index.js
@@ -1,12 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
 import { useNavigate } from "react-router-dom";
 import { sendVerifyAPIToAuthenticationServer } from "../../utils/util";
 
 // Import components
 import Navbar from "../common/Navbar";
+import Tile from "./Tile.js";
+
+// Define tiles
+const tilesData = [
+  {
+    title: "Tile 1",
+    description: "This is the first tile.",
+    urls: [
+      {
+        urlTitle: "Google",
+        url: "https://www.google.com",
+      },
+      {
+        urlTitle: "coinmarket Cap",
+        url: "https://coinmarketcap.com/coins",
+      },
+    ],
+  },
+  {
+    title: "Tile 2",
+    description: "This is the second tile.",
+    urls: [
+      {
+        urlTitle: "Google",
+        url: "https://www.google.com",
+      },
+      {
+        urlTitle: "coinmarket Cap",
+        url: "https://coinmarketcap.com/coins",
+      },
+    ],
+  },
+];
 
 function MultiTabOpener() {
   const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     // Check if JWT token exists
@@ -60,7 +100,44 @@ function MultiTabOpener() {
   return (
     <>
       <Navbar />
-      Welcome to Multi Tab Opener
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        style={{ marginBottom: "20px", marginTop: "20px" }}
+      >
+        <TextField
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          variant="outlined"
+          size="large" // You can adjust this to 'medium' if 'large' is too big.
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+          style={{ width: "50%" }} // Adjust the width of the search bar.
+        />
+      </Box>
+      <Container style={{ marginTop: "10px", marginBottom: "10px" }}>
+        <Grid container spacing={1}>
+          {tilesData
+            .filter((tile) =>
+              tile.title.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .map((tile, index) => (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                <Tile
+                  title={tile.title}
+                  description={tile.description}
+                  urls={tile.urls}
+                />
+              </Grid>
+            ))}
+        </Grid>
+      </Container>
     </>
   );
 }
