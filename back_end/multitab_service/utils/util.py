7e853da1ea8f1ecbf370dbdb02386b2c714d5735
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-07-19 17:36:44 -0500

    Implement backend for multitab lambda to invoke auth lambda

--- back_end/multitab_service/utils/util.py
+++ back_end/multitab_service/utils/util.py
@@ -1,5 +1,5 @@
 import json
-
+import boto3
 
 def buildResponse(code, message, jwt_token=""):
     
@@ -14,3 +14,35 @@ def buildResponse(code, message, jwt_token=""):
         'body': json.dumps(message)
 
     }
+
+def verifyAuthStatus(jwtToken):
+    # This function will invoke the auth lambda and verify the user auth status,
+    # If user is authenticated, returns true.
+    # If user is not authenticated, return false.
+    # throws an error to calling function if error occurred.
+
+    client = boto3.client('lambda')
+    try:
+        response = client.invoke(
+            FunctionName='password-generator-backend-lambda-auth-service',
+            InvocationType='RequestResponse',
+            Payload=json.dumps({
+                'httpMethod': 'POST',
+                'path': '/auth/verify',
+                'Authorization': 'Bearer ' + jwtToken
+                })
+        )
+        if 'FunctionError' in response:
+            error_response = json.load(response['Payload'])
+            print(f"Error type: {error_response['errorType']}")
+            print(f"Error message: {error_response['errorMessage']}")
+            raise Exception("Auth lambda encountered an error.")
+        
+        elif response['statusCode'] is 200:
+            return True
+        else:
+            return False
+        
+    except Exception as e:
+        print("Failed to get auth status of user")
+        raise Exception ("verifyAuthStatus(): " + str(e))
