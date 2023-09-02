54f417a488e200bf95ccc7cc8be529190c4994de
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-09-02 13:30:58 -0500

    added script to copy common to all backend folders

---
+++ .github/build_and_zip.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install python requirements
+python -m pip install -r requirements.txt -t ./python
+
+# Zip the python directory
+zip -r ../Infra/layer.zip python/
+
+# Remove the python directory
+rm -r python
+
+# List the contents of the current directory
+ls
+
+# Copy 'common' folder to all other folders in the same parent directory
+for dir in ../backend/
+do
+  if [ "$dir" != "./common/" ]; then
+    cp -r ./common/ "$dir"
+  fi
+done
+
+# List the contents of the parent directory
+ls ..
