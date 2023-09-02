8637a353f8583ccb00831ce714d842daffc6f34f
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-09-02 15:14:01 -0500

    Add pip install again

--- .github/scripts/build_and_zip.sh
+++ .github/scripts/build_and_zip.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 # Install python requirements
-#python -m pip install -r requirements.txt -t ./python
+python -m pip install -r requirements.txt -t ./python
 
 # Zip the python directory
-#zip -r ../Infra/layer.zip python/
+zip -r ../Infra/layer.zip python/
 
 # Remove the python directory
-#rm -r python
+rm -r python
 
 # List the contents of the current directory
 ls
 
 # Copy 'common' folder to all other folders in the backend directory
-
 for dir in ../back_end/
 do
-  if [ "$dir" != "./common/" ]; then
-    cp -r ./common/ "$dir"
+  if [ "$dir" != "../back_end/common/" ]; then
+    cp -r ../back_end/common/ "$dir"
   fi
 done
 
