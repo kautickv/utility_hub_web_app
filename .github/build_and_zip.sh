36ae3461dff3424315d4a8ea128e67eeb864620b
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-09-02 13:34:33 -0500

    Removing pip install for debugging purposes

--- .github/build_and_zip.sh
+++ .github/build_and_zip.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Install python requirements
-python -m pip install -r requirements.txt -t ./python
+#python -m pip install -r requirements.txt -t ./python
 
 # Zip the python directory
-zip -r ../Infra/layer.zip python/
+#zip -r ../Infra/layer.zip python/
 
 # Remove the python directory
-rm -r python
+#rm -r python
 
 # List the contents of the current directory
 ls
