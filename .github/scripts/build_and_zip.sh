8acde5cbceedaf3bfaa453643eb1b6592a44ea09
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-09-02 15:44:32 -0500

    Fixed bug in bash script. For loop is now looping in all directories

--- .github/scripts/build_and_zip.sh
+++ .github/scripts/build_and_zip.sh
@@ -3,25 +3,22 @@
 # Change to back_end directory
 cd ../back_end
 # Install python requirements
-#python -m pip install -r requirements.txt -t ./python
+python -m pip install -r requirements.txt -t ./python
 
 # Zip the python directory
-#zip -r ../Infra/layer.zip python/
+zip -r ../Infra/layer.zip python/
 
 # Remove the python directory
-#rm -r python
+rm -r python
 
-# List the contents of the current directory
-ls
-echo "This is branch"
 # Copy 'common' folder to all other folders in the backend directory
-for dir in ./
+for dir in ./*/
 do
+  echo "$dir"
   if [ "$dir" != "./common/" ]; then
     cp -r ./common/ "$dir"
-    echo "$dir"
-    cd "$dir"
-    ls
-    cd ..
   fi
 done
+
+# List the contents of the parent directory
+ls ..
