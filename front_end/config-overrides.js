49046e492d69c7e8a61320926880a75c3a0a0dec
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2023-09-07 14:57:14 -0500

    Change webpack configuration to create two seperate builds. One for main web app and one for chrome extension

---
+++ front_end/config-overrides.js
@@ -0,0 +1,37 @@
+// Config file to create two seperate builds for this react project,
+// One for the main web app
+// Another one for the chrome extension app
+const path = require('path');
+
+module.exports = function override(config, env) {
+  // Change the entry point
+  config.entry = {
+    main: path.join(__dirname, 'src/index.js'),
+    extension: path.join(__dirname, 'src/extensionApp.js'),
+  };
+
+  // Output multiple HTML files (one for each entry)
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+  config.plugins = config.plugins.map((plugin) => {
+    if (plugin instanceof HtmlWebpackPlugin) {
+      return new HtmlWebpackPlugin({
+        inject: true,
+        chunks: ['main'],
+        template: path.join(__dirname, 'public/index.html'),
+        filename: 'index.html',
+      });
+    }
+    return plugin;
+  });
+
+  config.plugins.push(
+    new HtmlWebpackPlugin({
+      inject: true,
+      chunks: ['extension'],
+      template: path.join(__dirname, 'public/extension.html'),
+      filename: 'extension.html',
+    })
+  );
+
+  return config;
+};
