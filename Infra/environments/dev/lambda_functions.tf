38ef8929df98ab801c3d36dc22eae1c90d77ba2f
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2024-02-21 17:26:11 -0600

    Added module and called module to create a lambda function

---
+++ Infra/environments/dev/lambda_functions.tf
@@ -0,0 +1,38 @@
+# CREATE LAMBDA FUNCTION FOR AUTH SERVICE
+##----------------------------------------------------------------------------------
+# Zip the current backend python script
+data "archive_file" "auth_lamda_function_zip" {
+  type = "zip"
+
+  source_dir  = "../${path.module}/back_end/auth_service"
+  output_path = "../${path.module}/auth_service.zip"
+}
+
+# Upload zip file to s3 bucket created earlier
+resource "aws_s3_object" "auth_lamda_function_object" {
+  bucket = module.lambda_zip_s3_bucket.bucket_id
+
+  key    = "auth_service.zip"
+  source = data.archive_file.auth_lamda_function_zip.output_path
+
+  etag = filemd5(data.archive_file.auth_lamda_function_zip.output_path)
+}
+
+module "auth_lamda_function" {
+  source = "../../modules/lambda/functions"
+
+  function_name = "Utility_hub_auth_service"
+  s3_bucket_id = module.lambda_zip_s3_bucket.bucket_id
+  s3_bucket_key = aws_s3_object.auth_lamda_function_object.key
+  handler_name = "index.lambda_handler"
+  source_code_hash = data.archive_file.auth_lamda_function_zip.output_base64sha256
+  role_arn = aws_iam_role.auth_lambda_exec_role.arn
+  layers_arn = [module.lambda_python_layer.layer_arn]
+  environment_variables = {
+    "MESSAGE"         = "Terraform sends its regards",
+    "USER_TABLE_NAME" = module.auth_dynamodb_table.table_name
+  }
+  timeout = 15
+  memory_size = 128
+}
+##----------------------------------------------------------------------------------
---
+++ Infra/modules/lambda/functions/variables.tf
@@ -0,0 +1,52 @@
+variable "function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+}
+
+variable "s3_bucket_id" {
+  description = "The S3 bucket ID where the Lambda function code is stored"
+  type        = string
+}
+
+variable "s3_bucket_key" {
+  description = "The S3 key of the Lambda function code"
+  type        = string
+}
+
+variable "handler_name" {
+  description = "The handler name of the Lambda function"
+  type        = string
+}
+
+variable "source_code_hash" {
+  description = "The base64-encoded SHA256 hash of the package file"
+  type        = string
+}
+
+variable "role_arn" {
+  description = "The ARN of the IAM role attached to the Lambda function"
+  type        = string
+}
+
+variable "layers_arn" {
+  description = "A list of Lambda layer ARNs to attach to the Lambda function"
+  type        = list(string)
+  default = []
+}
+
+variable "environment_variables" {
+  description = "A map of environment variables for the Lambda function"
+  type        = map(string)
+}
+
+variable "timeout" {
+  description = "The amount of time your Lambda Function has to run in seconds"
+  type        = number
+  default     = 15
+}
+
+variable "memory_size" {
+  description = "The amount of memory available to the Lambda function in MB"
+  type        = number
+  default     = 128
+}
