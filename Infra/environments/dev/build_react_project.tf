576ce73e4c551df2ea63a6fbc56cb0638231ae89
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2024-02-22 17:15:24 -0600

    Fix errors due to undeclared resources

--- Infra/environments/dev/build_react_project.tf
+++ Infra/environments/dev/build_react_project.tf
@@ -5,11 +5,11 @@
 # Create .env and store in front_end folder. Contains API gateway invoke url, s3 website endpoint.
 resource "local_file" "react_env_file" {
 
-  depends_on = [aws_api_gateway_stage.password_generator_api_gateway_stage]
+  depends_on = [aws_api_gateway_stage.utility_hub_api_gateway_stage]
   filename = "../${path.module}/front_end/.env"
   content  = <<-EOF
-    REACT_APP_API_GATEWAY_BASE_URL=${aws_api_gateway_stage.password_generator_api_gateway_stage.invoke_url}
-    REACT_APP_BUCKET_HOSTING_ID =${aws_s3_bucket.static_hosting_bucket_name.id}
+    REACT_APP_API_GATEWAY_BASE_URL=${aws_api_gateway_stage.utility_hub_api_gateway_stage.invoke_url}
+    REACT_APP_BUCKET_HOSTING_ID =${module.s3_static_hosting.bucket_id}
     REACT_APP_S3_WEBSITE_ENDPOINT =${aws_s3_bucket_website_configuration.static_hosting_bucket_config.website_endpoint}
 
     EOF
@@ -55,6 +55,6 @@ resource "null_resource" "sync_build_to_hosting_bucket" {
   }
 
   provisioner "local-exec" {
-    command = "aws s3 sync ../${path.module}/front_end/build 's3://${aws_s3_bucket.static_hosting_bucket_name.id}'"
+    command = "aws s3 sync ../${path.module}/front_end/build 's3://${module.s3_static_hosting.bucket_id}'"
   }
 }
