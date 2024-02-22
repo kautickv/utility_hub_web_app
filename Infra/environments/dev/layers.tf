5b08ba161b1418fd4802773e181839841274c327
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2024-02-21 18:32:30 -0600

    Fix file paths

--- Infra/environments/dev/layers.tf
+++ Infra/environments/dev/layers.tf
@@ -4,6 +4,6 @@ module "lambda_python_layer" {
 
     layer_s3_bucket_id = module.lambda_zip_s3_bucket.bucket_id
     key = "layer.zip"
-    file_source = "${path.module}/layer.zip"
+    file_source = "../../${path.module}/layer.zip"
     layer_name = "Python-layer-Utility_Hub"
 }
