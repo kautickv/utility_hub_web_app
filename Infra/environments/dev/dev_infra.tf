4c958d3ef0b578911ba066c235a12c721af645c2
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2024-02-25 22:53:44 -0600

    Pass providers to module

--- Infra/environments/dev/dev_infra.tf
+++ Infra/environments/dev/dev_infra.tf
@@ -3,6 +3,12 @@ module "build_dev_infra" {
 
     source = "../../modules/shared"
 
+    providers = {
+        aws.default = aws.default
+        aws.acm   = aws.acm
+        aws.dns_account = aws.dns_account
+    }
+
     app_name = var.app_name
     bucket_name = var.bucket_name
     domain_name = var.domain_name
