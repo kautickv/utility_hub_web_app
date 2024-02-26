18ebb9c4e71a1a1790f1a40ff92fe135a4050527
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2024-02-25 22:47:48 -0600

    rename provider file main

--- Infra/environments/dev/main.tf
+++ Infra/environments/dev/main.tf
@@ -1,10 +1,44 @@
-## Build entire Dev Infra
-module "build_dev_infra" {
+terraform {
 
-    source = "../../modules/shared"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.21.0"
+    }
 
-    app_name = var.app_name
-    bucket_name = var.bucket_name
-    domain_name = var.domain_name
-    hosted_zone_id= var.hosted_zone_id
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "utility-hub-s3-terraform-backend" # Create bucket prior to when script runs. State will be stored to this bucket. Add necessary permissions to bucket and user/role
+    key            = "terraformStateFiles" # Folder structure to where exactly to store state
+    region         = "us-east-1"
+  }
+}
+
+# Default AWS Account
+provider "aws" {
+  # Use default credentials
+  region = var.aws_default_region
+}
+
+# Used only for Certificate manager
+provider "aws" {
+  # use default credentials
+  alias = "acm"
+  region = "us-east-1"
+
+}
+
+#Provider for DNS Account setup
+provider "aws"{
+  alias = "dns_account"
+  region = var.dns_account_region
+
+  access_key = var.dns_aws_access_key_id
+  secret_key = var.dns_aws_secret_access_key
+  token      = var.dns_aws_session_token
 }
