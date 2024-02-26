1bae778781a2468ae3090a6b85ca7b357ec2a310
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2024-02-25 20:40:09 -0600

    Moved all tf files into shared folder to use as modules for dev and prod deployments

---
+++ Infra/modules/shared/terraform.tfvars
@@ -0,0 +1,4 @@
+bucket_name = "utilityhub.vaisnavsingkautick.com"
+domain_name = "utilityhub.vaisnavsingkautick.com"
+hosted_zone_id = "Z0236168HPT8WHQWIY9"
+app_name = "utility-hub"
---
+++ Infra/modules/shared/variables.tf
@@ -0,0 +1,36 @@
+variable "bucket_name" {
+  description = "Name of S3 Static bucket hosting"
+  type        = string
+}
+
+variable "domain_name" {
+    description = "Domain name of website"
+    type = string
+}
+
+variable "hosted_zone_id"{
+    description = "The ID for the hosted zone"
+    type = string
+}
+
+variable "app_name"{
+  description = "The name of the application"
+  type = string
+  default = "Utility_hub"
+}
+
+variable "aws_access_key_id_2" {
+  description = "AWS Access Key ID for Account 2"
+  type        = string
+}
+
+variable "aws_secret_access_key_2" {
+  description = "AWS Secret Access Key for Account 2"
+  type        = string
+}
+
+variable "aws_session_token_2" {
+  description = "AWS Session Token for Account 2"
+  type        = string
+}
+
