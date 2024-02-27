bfff52343fc1ab870b98c34d5cb3b587c272c18c
Vaisnavsing <83382963+kautickv@users.noreply.github.com>
2024-02-26 19:59:58 -0600

    Fix backend S3 bucket duplicate name error (#35)
    * Renamed backend S3 bucket

--- Infra/modules/shared/storage.tf
+++ Infra/modules/shared/storage.tf
@@ -1,6 +1,6 @@
 # Create an s3 Bucket to store the zip files to upload to lambda
 module "lambda_zip_s3_bucket"{
     source = "../../modules/s3"
-    bucket_name = "${var.app_name}-lambda-python-backend-code-s3-bucket"
+    bucket_name = "${var.domain_name}-backend-code-s3-bucket"
     force_destroy = true
 }
