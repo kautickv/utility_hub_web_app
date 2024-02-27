080300ea5df2f9700d416d5f30cc378695d8bb40
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2024-02-26 18:46:33 -0600

    Added prod terraform configurations

---
+++ Infra/environments/prod/prod_infra.tf
@@ -0,0 +1,15 @@
+## Build entire Dev Infra
+module "build_prod_infra" {
+
+    source = "../../modules/shared"
+
+    providers = {
+        aws.acm   = aws.acm
+        aws.dns_account = aws.dns_account
+    }
+
+    app_name = var.app_name
+    bucket_name = var.bucket_name
+    domain_name = var.domain_name
+    hosted_zone_id= var.hosted_zone_id
+}
