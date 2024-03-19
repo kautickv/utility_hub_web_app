2da1ac6c6310da69b555b58cd2bd03a0a8b32dce
Vashish <kautickv29@gmail.com>
2024-03-19 12:33:02 -0500

    Added backend index.py

---
+++ back_end/json_viewer_service/index.py
@@ -0,0 +1,36 @@
+from common.CommonUtility import CommonUtility
+from common.Logger import Logger
+
+def lambda_handler(event, context):
+    print(event)
+    http_method = event['httpMethod']
+    path = event['path']
+
+    # Initialize CommonUtility Class
+    common_utility = CommonUtility()
+    
+    try:
+        # Extract auth code
+        code = common_utility.getAuthorizationCode(event)
+        if code is None:
+            return common_utility.buildResponse(401, "Unauthorized")
+        else:
+            ## VerifyAuthStatus will also return the user details associated with JWT Token
+            user_details = common_utility.verifyAuthStatus(code)
+            if user_details == None:
+                return common_utility.buildResponse(401, "Unauthorized")
+            
+        if http_method == 'POST' and path =='/json_viewer':
+            return common_utility.buildResponse(200, "OK from POST /json_viewer")
+    
+        elif http_method == 'GET' and path =='/json_viewer':
+
+            return common_utility.buildResponse(200, "OK from GET /json_viewer")
+        else:
+            return common_utility.buildResponse(404, "Resource not found")
+    
+    except Exception as e:
+        #Initialise Logging instance
+        logging_instance = Logger()
+        logging_instance.log_exception(e, 'json_viewer_service_handler')
+        return common_utility.buildResponse(500, "An error occurred. Please try again later")
