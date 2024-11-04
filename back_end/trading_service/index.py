bae11e476f88647694f81ed7daac41b186ad653b
kautick-vaisnavsing <kautick.vaisnavsing@skipthedishes.ca>
2024-11-04 17:18:06 -0600

    Added eventbridge trigger

--- back_end/trading_service/index.py
+++ back_end/trading_service/index.py
@@ -10,38 +10,45 @@ def lambda_handler(event, context):
     try:
         #initialise CommonUtility Class
         common_utility = CommonUtility()
-        ## Check if user is authenticated
-        code = common_utility.getAuthorizationCode(event)
-        if code is None:
-            return common_utility.buildResponse(401, "Unauthorized")
-        else:
-            ## VerifyAuthStatus will also return the user details associated with JWT Token
-            user_details = common_utility.verifyAuthStatus(code)
-            if user_details == None:
+        if "httpMethod" in event:
+            ## Check if user is authenticated
+            code = common_utility.getAuthorizationCode(event)
+            if code is None:
                 return common_utility.buildResponse(401, "Unauthorized")
-            
-            if user_details['email'] != 'kautickv29@gmail.com':
-                return common_utility.buildResponse(403, "Forbidden")
+            else:
+                ## VerifyAuthStatus will also return the user details associated with JWT Token
+                user_details = common_utility.verifyAuthStatus(code)
+                if user_details == None:
+                    return common_utility.buildResponse(401, "Unauthorized")
+                
+                if user_details['email'] != 'kautickv29@gmail.com':
+                    return common_utility.buildResponse(403, "Forbidden")
         
-        http_method = event['httpMethod']
-        path = event['path']
-        # Parse the JSON body
-        body = event.get('body', '{}')
-        body_params = json.loads(body) if body else {}
+            http_method = event['httpMethod']
+            path = event['path']
+            # Parse the JSON body
+            body = event.get('body', '{}')
+            body_params = json.loads(body) if body else {}
 
-        if http_method == 'POST' and path =='/trading':
+            if http_method == 'POST' and path =='/trading':
+                
+                return handlePostTrading(body_params)
             
-            return handlePostTrading(body_params)
-        
-        elif http_method == 'GET' and path =='/trading':
+            elif http_method == 'GET' and path =='/trading':
 
-            query_params = event.get('queryStringParameters', {})
-            return handleGetTrading(query_params)
-        elif body.get('action', {}) == "auto":
-            # Lambda triggered by eventbridge.
-            return handleAutoTrading(body)
-        else:
-            return common_utility.buildResponse(404, "Resource not found")
+                query_params = event.get('queryStringParameters', {})
+                return handleGetTrading(query_params)
+            else:
+                return common_utility.buildResponse(404, "Resource not found")
+        
+        # Check if triggered by EventBridge
+        elif "source" in event and event["source"] == "aws.events":
+            params = event.get('detail', {})
+            action = params.get('action', None)
+            if action == "auto":
+                return handleAutoTrading(event)
+            else:
+                return "Unknown parameters"
     
     except Exception as e:
         print(f"An error occurred: ${e}")
